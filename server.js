cba6657f4d691409bdb2c28ecb3298841c0390ae
Aaron Greene <AGreene@technip.com>
2017-12-14 17:51:56 -0330

    added callback route

--- server.js
+++ server.js
@@ -18,6 +18,55 @@ app.get('/', function (req, res) {
   res.send('vue-authenticate')
 })
 
+app.get('/auth/yahoo/callback', function(req, res) {
+    var accessTokenUrl = 'https://api.login.yahoo.com/oauth2/get_token';
+  
+    var options = {
+      url: accessTokenUrl,
+      headers: { Authorization: 'Basic ' + new Buffer(clientId + ':' + clientSecret).toString('base64') },
+      rejectUnauthorized: false,
+      json: true,
+      form: {
+        code: req.query.code,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code'
+      }
+    };
+  
+    // 1. Exchange authorization code for access token.
+    request.post(options, function(err, response, body) {
+      var guid = body.xoauth_yahoo_guid;
+      var accessToken = body.access_token;
+      var socialApiUrl = 'https://social.yahooapis.com/v1/user/' + guid + '/profile?format=json';
+  
+      var options = {
+        url: socialApiUrl,
+        headers: { Authorization: 'Bearer ' + accessToken },
+        rejectUnauthorized: false,
+        json: true
+      };
+  
+      // 2. Retrieve profile information about the current user.
+      request.get(options, function(err, response, body) {
+  
+      // 3. Create a new user account or return an existing one
+  
+        var user = {
+          guid: guid,
+          //email: body.profile.emails[0].handle,
+          profileImage: body.profile.image.imageUrl,
+          firstName: body.profile.givenName,
+          lastName: body.profile.familyName,
+          displayName: body.profile.nickname,
+          accessToken: accessToken
+        };
+        req.session.user = user;
+        res.redirect('/');
+  
+      });
+    });
+  });
+
 app.post('/auth/:provider', function(req, res){
   switch(req.params.provider) {
     case 'facebook':
